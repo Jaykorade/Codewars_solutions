e0240decb718a9ad187c077e8bd1f68a931edf4b
Jaykorade <jaykorade@gmail.com>
2024-08-11 14:28:20 +0530

    commitme

--- what_day.py
+++ what_day.py
@@ -7,7 +7,8 @@ def whatday(num):
         elif num > 7 and num < 1:
             return "Wrong, please enter a number between 1 and 7"
 
+#revised
 
 def whatday(num):
     Days = ['Sun','Mon','Tues','Wednes','Thurs','Fri','Satur']
-    return Days[num-1] + "day" if 1 <= num <= 7 else "Wrong, please enter a number between 1 and 7"
+    return Days[num-1] + "day" if 1 <= num <= 7 else "Wrong, please enter a number between 1 and 7" 
